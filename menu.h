cd9956bf7c95661d0e1ecfcbe3eda64268666db1
Audrey Der <ader003@ucr.edu>
2017-02-27 18:49:38 -0800

    Menu.h draft 1

--- menu.h
+++ menu.h
@@ -12,28 +12,85 @@ class Menu {
 		vector<Command*> history;
 
 	public:
-		Menu() {
+		Menu()
+		{
 			//Base constructor to set up all necessary members
+			history_index = 0;	
 		};
-		void execute() {
+		
+		void execute()
+		{
 			//Prints the current commands value (based on history_index), if no commands exist
 			//print 0 by default
+			if (history_index >= 0)
+			{
+				cout << history.at(history_index) << endl;
+			}
+
+			else
+			{
+				cout << 0 << endl;
+			}
 		};
-		bool initialized() {
+
+		bool initialized()
+		{
 			//Return true if the history has been primed with a single op instruciton
 			//This is necessary because that is the base of a calculation
+			
+			if (history_index >= 0)
+			{
+				return true;
+			}
+
+			else
+			{
+				return false;
+			}
 		};
-		void add_command(Command* cmd) {
+		
+		void add_command(Command* cmd)
+		{
 			//Adds a command to history in the appropriate posiiton (based on history_index)
+			history.at(history_index + 1) = cmd;
+			return;
 		};
-		Command* get_command() {
+
+		Command* get_command()
+		{
 			//Returns the command in history we are currently referring to (based on history_index)
+			return history.at(history_index);
+
 		};
-		void undo() {
+
+		void undo()
+		{
 			//Moves to the last command in history (if possible)
+			if(history.size() > 0)
+			{
+				history_index = history_index - 1;	
+			}
+
+			else
+			{
+				cout << "Error undoing." << endl;
+			}
+			
 		};
-		void redo() {
+
+		void redo()
+		{
 			//Moves to the next command in history (if possible)
+			
+			if(history.size() >= static_cast<unsigned>history_index)
+			{
+				history_index = history_index + 1;
+			}
+
+			else
+			{
+				cout << "Error redoing." << endl;
+			}
 		};
 };
 
